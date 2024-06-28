8a8d2c126a797b938cf101bff96569e58af2c95e
Jake <88286662+JinternalZ@users.noreply.github.com>
2024-06-28 13:46:28 -0500

    Commit 1

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import nuxt from "eslint-plugin-nuxt";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      nuxt,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...nuxt.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+    },
+  },
+];
